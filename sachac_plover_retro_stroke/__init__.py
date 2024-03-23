2462acf9a9ecab53d646a5254e7432644d8cb395
josiah <josiah123t@gmail.com>
2024-03-23 14:22:43 +1100

    I don't know what this is, just committing

--- sachac_plover_retro_stroke/__init__.py
+++ sachac_plover_retro_stroke/__init__.py
@@ -3,16 +3,24 @@
 from typing import *
 import itertools
 from plover.translation import Translator, Stroke, Translation
+from plover.formatting import RetroFormatter
+import re
 
 def flatten(x: List[List]) -> List:
     return list(itertools.chain.from_iterable(x))
 
-def retro_stroke(translator: Translator, stroke: Stroke, cmdline: str):
-    args = cmdline.split(",")
-    separator = args[0] if len(args) > 0 else " "
-    before = args[1] if len(args) > 1 else ""
-    after = args[2] if len(args) > 2 else ""
+historical_translations = []
 
+def expandFormatting(format_string, items): 
+    # thanks for this rabbit growth!
+    def replace(matchobj):
+        width, letter = matchobj.groups()
+        width = 0 if not width else int(width)
+        return items.get(letter, '').ljust(width)
+    return re.sub(r'%(\d*)(.)', replace, format_string)
+
+def retro_stroke(translator: Translator, stroke: Stroke, cmdline: str):
+    formatting = cmdline
     all_translations = translator.get_state().translations
     affected_translation_cnt = len(list(
         itertools.takewhile(
@@ -21,13 +29,27 @@ def retro_stroke(translator: Translator, stroke: Stroke, cmdline: str):
         )
     ))
     affected_translations = all_translations[-(affected_translation_cnt + 1):]
+    if len(affected_translations) >= 1:
+        affected_english = affected_translations[0]
+        global historical_translations
+        if affected_translation_cnt:
+            historical_translations = [affected_english] + historical_translations 
+        else:
+            historical_translations = [affected_english]
+        translated = ''.join(reversed(list(RetroFormatter(historical_translations).iter_last_fragments())))
+    else:
+        translated = ""
+    
     affected_strokes = flatten([x.strokes for x in affected_translations])
-    result = separator.join([x.rtfcre for x in reversed(list(itertools.dropwhile(
+    raw_steno = "/".join([x.rtfcre for x in reversed(list(itertools.dropwhile(
         lambda x: x == stroke,
         reversed(affected_strokes)
     )))])
-    my_trans = Translation(affected_strokes + [stroke], before + result + after)
+    items = {
+        'r': raw_steno,
+        'T': translated,
+        '%': '%'}
+    formatted_result = expandFormatting(formatting, items)
+    my_trans = Translation(affected_strokes + [stroke], formatted_result)
     my_trans.replaced = affected_translations
     translator.translate_translation(my_trans)
-
-
